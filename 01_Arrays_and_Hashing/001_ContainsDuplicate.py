ec439a52c2936eb29af6edcc529c66578afec4e2
majikojima <ykk61890@yahoo.co.jp>
2023-07-22 09:01:47 +0900

    add 001_ContainsDuplicate.py

---
+++ 01_Arrays_and_Hashing/001_ContainsDuplicate.py
@@ -0,0 +1,47 @@
+# [https://leetcode.com/problems/contains-duplicate/]
+
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+
+# クラスのインスタンスを作成
+s = Solution()
+
+# ファイルから数値のリストを読み込む
+with open('./input/001.txt', 'r') as f:
+    for line in f:
+        nums = [int(num) for num in line.strip().split(',')]
+        # 各行（テストケース）ごとにメソッドを呼び出し、結果を出力する
+        result = s.containsDuplicate(nums)
+        print(nums)
+        print(result)
+
+"""
+Pythonのこのコードは、リスト`nums`内に重複する要素が存在するかを確認するものです。詳細については以下の通りです：
+
+1. `class Solution:`: Solutionという名前の新しいクラスを定義します。Pythonでは、オブジェクト指向プログラミングを可能にするためにクラスが使用されます。
+
+2. `def containsDuplicate(self, nums: List[int]) -> bool:`: このクラス内にメソッド`containsDuplicate`を定義します。このメソッドは`self`（自身のオブジェクトを参照）と`nums`（intのリスト）を引数に取り、bool型（TrueまたはFalse）の結果を返します。
+
+3. `hashset = set()`: 空の集合`hashset`を初期化します。集合は、重複した要素を保持しない特性を持つデータ型です。
+
+4. `for n in nums:`: 引数として渡されたリスト`nums`の各要素についてループを行います。各ループでの要素は`n`として参照されます。
+
+5. `if n in hashset:`: もし`n`が`hashset`の中に存在していたら（つまり、前に同じ要素を見つけていたら）…
+
+6. `return True`: ...すぐに`True`を返します。これは、リストに重複した要素が存在することを意味します。
+
+7. `hashset.add(n)`: 一方、`n`が`hashset`に存在しない場合は、`n`を`hashset`に追加します。
+
+8. `return False`: ループが完全に終了した後（すなわち、リスト内のすべての要素が一度だけ見られた後）、`False`を返します。これは、リストに重複した要素が存在しないことを意味します。
+
+このコードの全体的な目的は、与えられたリスト内に重複する要素が存在するかどうかを高速に判断することです。そして、それはPythonの集合の特性と、特にハッシュベースの実装により、O(1)の時間複雑度で`in`演算を行うことが可能なため、可能となります。
+"""
