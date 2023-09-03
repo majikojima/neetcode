9d04518bc22b0ebfb0b1e61ff5d09b09d76417f6
majikojima <ykk61890@yahoo.co.jp>
2023-09-03 22:49:59 +0900

    pra

---
+++ 00_practice/04_06.py
@@ -0,0 +1,9 @@
+from typing import List
+    
+def carFleet(target: int, position: List[int], speed: List[int]) -> int:
+
+
+target = 12
+position = [10,8,0,5,3]
+speed = [2,4,1,1,3]
+print(carFleet(target, position, speed))
