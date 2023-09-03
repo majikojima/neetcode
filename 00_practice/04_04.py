3e29367aeed5541cc1448cd3a9de38dde0c7290e
majikojima <ykk61890@yahoo.co.jp>
2023-09-03 22:20:26 +0900

    pra

---
+++ 00_practice/04_04.py
@@ -0,0 +1,7 @@
+from typing import List
+
+def generateParenthesis(n: int) -> List[str]:
+
+
+n = 3
+print(generateParenthesis(n))
---
+++ 00_practice/04_05.py
@@ -0,0 +1,7 @@
+from typing import List
+
+def dailyTemperatures(temperatures: List[int]) -> List[int]:
+
+
+temperatures = [73,74,75,71,69,72,76,73]
+print(dailyTemperatures(temperatures))
