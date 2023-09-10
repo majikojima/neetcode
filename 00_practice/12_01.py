0b69fbe64fb43e5a61ef01631506e1ad365c01c6
majikojima <ykk61890@yahoo.co.jp>
2023-09-10 21:48:07 +0900

    add 12_01

---
+++ 00_practice/12_01.py
@@ -0,0 +1,11 @@
+from typing import List
+import heapq
+
+def minCostConnectPoints(points: List[List[int]]) -> int:
+    
+
+points = [[0,0],[2,2],[3,10],[5,2],[7,0]]
+print(minCostConnectPoints(points))
+
+points = [[3,12],[-2,5],[-4,1]]
+print(minCostConnectPoints(points))
