e418ee9ddb927e9816a4144cf69333808167bf36
majikojima <ykk61890@yahoo.co.jp>
2023-09-14 14:55:01 +0900

    pra

---
+++ 00_practice/15_02.py
@@ -0,0 +1,9 @@
+from typing import List
+
+def canJump(nums: List[int]) -> bool:
+        
+nums = [2,3,1,1,4]
+print(canJump(nums))
+
+nums = [3,2,1,0,4]
+print(canJump(nums))
---
+++ 00_practice/15_03.py
@@ -0,0 +1,9 @@
+from typing import List
+
+def jump(nums: List[int]) -> bool:
+        
+nums = [2,3,1,1,4]
+print(jump(nums))
+
+nums = [2,3,0,1,4]
+print(jump(nums))
