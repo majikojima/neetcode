598459e3782c457b29dd6879d58f7e82fb78d267
majikojima <ykk61890@yahoo.co.jp>
2023-09-21 16:40:56 +0900

    pra

---
+++ 00_practice/13_11.py
@@ -0,0 +1,12 @@
+from typing import List
+
+def lengthOfLIS(nums: List[int]) -> int:
+
+nums = [10,9,2,5,3,7,101,18]
+print(lengthOfLIS(nums))
+
+nums = [0,1,0,3,2,3]
+print(lengthOfLIS(nums))
+
+nums = [7,7,7,7,7,7,7]
+print(lengthOfLIS(nums))
