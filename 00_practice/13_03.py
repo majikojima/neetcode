dfc4d7fbaa3051084dacdee064ca5b43c6c3da5e
majikojima <ykk61890@yahoo.co.jp>
2023-09-26 09:24:41 +0900

    pra

---
+++ 00_practice/13_03.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def rob(nums: List[int]) -> int:
+
+
+nums = [1,2,3,1]
+print(rob(nums))
+
+nums = [7,2,9,3,1,100]
+print(rob(nums))
---
+++ 00_practice/13_04.py
@@ -0,0 +1,12 @@
+from typing import List
+
+def rob(nums: List[int]) -> int:
+
+nums = [2,3,2]
+print(rob(nums))
+
+nums = [1,2,3,1]
+print(rob(nums))
+
+nums = [1,2,3]
+print(rob(nums))
